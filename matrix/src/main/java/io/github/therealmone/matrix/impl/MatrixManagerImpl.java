a384a549891ee8afbd9104f13ad9f7805910166e
Vladimir Martyanov <v.s.martyanov@yandex.ru>
2018-11-20 00:37:26 +0300

    MatrixManagerImpl реализован

--- matrix/src/main/java/io/github/therealmone/matrix/impl/MatrixManagerImpl.java
+++ matrix/src/main/java/io/github/therealmone/matrix/impl/MatrixManagerImpl.java
@@ -10,27 +10,61 @@ public class MatrixManagerImpl implements MatrixManager {
 
     @Override
     public Matrix matrixProduct(Matrix firstMatrix, Matrix secondMatrix) {
-        return null;
+        if(firstMatrix.getColumnCount() == secondMatrix.getRowCount()){
+            return null;
+        }
+        Matrix matrixProduct = new Matrix(firstMatrix.getRowCount(), secondMatrix.getColumnCount());
+        for(int i = 0; i < firstMatrix.getRowCount(); i++){ //каждую строку из первой матрицы
+            for(int k = 0; k < secondMatrix.getColumnCount(); k++){ //умножаю на каждый столбец из второй матрицы
+                double multiplyRowByColumnResult = 0;
+                for (int l = 0; l < firstMatrix.getColumnCount(); l++) { // складывая произведения элементов
+                    multiplyRowByColumnResult += firstMatrix.getValue(i,l) * secondMatrix.getValue(l,k);
+                }
+                matrixProduct.setValue(i,k,multiplyRowByColumnResult); //
+            }
+        }
+        return matrixProduct;
     }
 
     @Override
     public Matrix transpose(Matrix matrix) {
-        return null;
+        Matrix transposeMatrix = new Matrix(matrix.getColumnCount(), matrix.getRowCount());
+        for(int i = 0; i < matrix.getRowCount(); i++){ //для каждой строки матрицы
+            for(int k = 0; k < matrix.getColumnCount(); k++){ //берем каждый элемент
+                transposeMatrix.setValue(k, i, matrix.getValue(i,k));
+            }
+        }
+        return transposeMatrix;
     }
 
     @Override
     public Matrix scalar(Matrix matrix, Double value) {
-        return null;
+        //нужно ли создавать новую матрицу? если нет, то не будет ли изменена сама передаваемая матрица в вызывающем методе?
+        for(int i = 0; i < matrix.getRowCount(); i++){
+            for (int k = 0; k < matrix.getColumnCount(); k++){
+                matrix.setValue(i,k,matrix.getValue(i,k) * value);
+            }
+        }
+        return matrix;
     }
 
     @Override
     public Matrix add(Matrix matrix, Double value) {
-        return null;
+        for(int i = 0; i < matrix.getRowCount(); i++){
+            for (int k = 0; k < matrix.getColumnCount(); k++){
+                matrix.setValue(i,k,matrix.getValue(i,k) + value);
+            }
+        }
+        return matrix;
     }
 
     @Override
     public Matrix fromArray(double[] array) {
-        return null;
+        Matrix matrix = new Matrix(array.length);
+        for(int i = 0; i < array.length; i++){
+            matrix.setValue(i,array[i]);
+        }
+        return matrix;
     }
 
     @Override
